3038e7aca7cb0690b4def41458653d3a78922f9b
pedro.almeida <pedro.almeida@rtcom.pt>
2025-02-14 10:32:51 +0000

    Add url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"https://pedrojcalmeida.github.io/portifolio-react",
+  base:"https://pedrojcalmeida.github.io/portfolio",
   plugins: [react(),tailwindcss()],
   
 })
